24e6e654625b8a15daf13787b1fedf4051fe19cb
sergei <s-kanaev@@users.noreply.github.com>
2022-04-25 01:30:52 +0300

    Add constants.hpp to hold FW specific consts

---
+++ analysis/e591/contants.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <types.hpp>
+
+namespace Const {
+// Synchronization disc teeth numbers start from 0
+static constexpr byte SyncDiscTeethCount = 60;
+static constexpr byte SyncDiscLastTooth = SyncDiscTeethCount - 1;
+static constexpr byte SyncDiscMissingTeethCount = 2;
+static constexpr byte SyncDiscLastRealTooth =
+    SyncDiscTeethCount - SyncDiscMissingTeethCount - 1;
+static constexpr byte SyncDiscLastRealToothMinus1 = SyncDiscLastRealTooth - 1;
+} // namespace Const
